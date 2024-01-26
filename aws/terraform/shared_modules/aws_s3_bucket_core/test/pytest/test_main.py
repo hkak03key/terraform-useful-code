62471dcfc1f9f43689098b41f9c7538063de8d9a
Hikaru <hkak03key@gmail.com>
2024-01-26 08:59:23 +0000

    update test using class

--- aws/terraform/shared_modules/aws_s3_bucket_core/test/pytest/test_main.py
+++ aws/terraform/shared_modules/aws_s3_bucket_core/test/pytest/test_main.py
@@ -1,9 +1,12 @@
 import logging
 import os
 import uuid
+from abc import ABC, abstractmethod
+from functools import singledispatchmethod
 
 import boto3
 from tf_pytest.aws import generate_boto3_session
+from typing_extensions import override
 
 import pytest
 
@@ -12,143 +15,143 @@ _logger = logging.getLogger(__name__)
 _logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
 
 
-@pytest.fixture(
-    params=[
-        {
-            "iam_role": "read",
-            "result": False,
-        },
-        {
-            "iam_role": "readwrite",
-            "result": True,
-        },
-        {
-            "iam_role": "not_attached",
-            "result": False,
-        },
-    ]
+class AwsIAMPolicyTester(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def __del__(self):
+        pass
+
+    @singledispatchmethod
+    @abstractmethod
+    def test(self):
+        pass
+
+
+class AwsIAMPolicyTesterS3Write(AwsIAMPolicyTester):
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+    @singledispatchmethod
+    @override
+    def test(self):
+        raise NotImplementedError(f"This {type(self).__name__} does not implement test().")
+
+    @test.register
+    def _test_impl(self, iam_role_name: str, bucket_name: str, object_key: str) -> bool:
+        try:
+            session = generate_boto3_session(iam_role_name)
+            s3 = session.client("s3")
+            s3.put_object(Bucket=bucket_name, Key=object_key, Body="test")
+            s3.delete_object(Bucket=bucket_name, Key=object_key)
+            return True
+        except Exception as e:
+            _logger.error(e)
+            return False
+
+
+@pytest.mark.parametrize(
+    "iam_role, expect",
+    [
+        (d["iam_role"], d["expect"])
+        for d in [
+            {
+                "iam_role": "read",
+                "expect": False,
+            },
+            {
+                "iam_role": "readwrite",
+                "expect": True,
+            },
+            {
+                "iam_role": "not_attached",
+                "expect": False,
+            },
+        ]
+    ],
 )
-def generate_params_for_test_write_object(tfstate_skip_apply, request):
+def test_write_object(tfstate_skip_apply, request, iam_role, expect):
+    root = tfstate_skip_apply
+
+    aws_s3_bucket = root.module.default.aws_s3_bucket.default
+    aws_iam_role = root.aws_iam_role.defaults[iam_role]
+
+    object_key = str(uuid.uuid4())
+
+    tester = AwsIAMPolicyTesterS3Write()
+
+    assert tester.test(aws_iam_role.values["arn"], aws_s3_bucket.values["bucket"], object_key) == expect
+
+
+class AwsIAMPoliyTesterS3Read(AwsIAMPolicyTester):
+    def __init__(self, aws_s3_bucket_name: str, aws_s3_object_key: str):
+        _logger.info("__init__()")
+        self._aws_s3_bucket_name = aws_s3_bucket_name
+        self._aws_s3_object_key = aws_s3_object_key
+
+        client = boto3.client("s3")
+        client.put_object(Bucket=self._aws_s3_bucket_name, Key=self._aws_s3_object_key, Body="test")
+
+    def __del__(self):
+        _logger.info("__del__()")
+        client = boto3.client("s3")
+        client.delete_object(Bucket=self._aws_s3_bucket_name, Key=self._aws_s3_object_key)
+
+    @singledispatchmethod
+    @override
+    def test(self):
+        raise NotImplementedError(f"This {type(self).__name__} does not implement test().")
+
+    @test.register
+    def _test_impl(self, iam_role_name: str):
+        try:
+            session = generate_boto3_session(iam_role_name)
+            s3 = session.client("s3")
+            s3.get_object(Bucket=self._aws_s3_bucket_name, Key=self._aws_s3_object_key)
+            return True
+        except Exception as e:
+            _logger.error(e)
+            return False
+
+
+@pytest.mark.parametrize(
+    "iam_role, expect",
+    [
+        (d["iam_role"], d["expect"])
+        for d in [
+            {
+                "iam_role": "read",
+                "expect": True,
+            },
+            {
+                "iam_role": "readwrite",
+                "expect": True,
+            },
+            {
+                "iam_role": "not_attached",
+                "expect": False,
+            },
+        ]
+    ],
+)
+def test_read_object(tfstate_skip_apply, request, iam_role, expect):
     root = tfstate_skip_apply
 
     object_key = str(uuid.uuid4())
 
-    params = {
-        "pattern": request.param,
-        "result": request.param["result"],
-        "kwargs": {
-            "aws_s3_bucket": root.module.default.aws_s3_bucket.default,
-            "aws_iam_role": root.aws_iam_role.defaults[request.param["iam_role"]],
-            "object_key": object_key,
-        },
-    }
+    aws_s3_bucket = root.module.default.aws_s3_bucket.default
+    aws_iam_role = root.aws_iam_role.defaults[iam_role]
 
-    yield params
-
-    # 後始末
-    client = boto3.client("s3")
-    client.delete_object(Bucket=params["kwargs"]["aws_s3_bucket"].values["bucket"], Key=params["kwargs"]["object_key"])
-
-
-def is_possible_to_write_object(session, bucket_name, object_key):
-    try:
-        s3 = session.client("s3")
-        # put
-        s3.put_object(Bucket=bucket_name, Key=object_key, Body="test")
-        # delete
-        s3.delete_object(Bucket=bucket_name, Key=object_key)
-        return True
-
-    except Exception as e:
-        _logger.error(e)
-        return False
-
-
-def test_write_object(generate_params_for_test_write_object):
-    _logger.info("test_put_log_events")
-
-    params = generate_params_for_test_write_object
-    pattern = params["pattern"]
-
-    _logger.info({"pattern": pattern})
-
-    session = generate_boto3_session(params["kwargs"]["aws_iam_role"].values["arn"])
-
-    result = is_possible_to_write_object(
-        session, params["kwargs"]["aws_s3_bucket"].values["bucket"], params["kwargs"]["object_key"]
+    tester = AwsIAMPoliyTesterS3Read(
+        **{
+            "aws_s3_bucket_name": aws_s3_bucket.values["bucket"],
+            "aws_s3_object_key": object_key,
+        }
     )
 
-    assert result == params["result"]
-
-
-@pytest.fixture(
-    params=[
-        {
-            "iam_role": "read",
-            "result": True,
-        },
-        {
-            "iam_role": "readwrite",
-            "result": True,
-        },
-        {
-            "iam_role": "not_attached",
-            "result": False,
-        },
-    ]
-)
-def generate_params_for_test_read_object(tfstate_skip_apply, request):
-    root = tfstate_skip_apply
-
-    object_key = str(uuid.uuid4())
-
-    params = {
-        "pattern": request.param,
-        "result": request.param["result"],
-        "kwargs": {
-            "aws_s3_bucket": root.module.default.aws_s3_bucket.default,
-            "aws_iam_role": root.aws_iam_role.defaults[request.param["iam_role"]],
-            "object_key": object_key,
-        },
-    }
-
-    # 準備
-    client = boto3.client("s3")
-    client.put_object(
-        Bucket=params["kwargs"]["aws_s3_bucket"].values["bucket"], Key=params["kwargs"]["object_key"], Body="test"
-    )
-
-    yield params
-
-    # 後始末
-    client = boto3.client("s3")
-    client.delete_object(Bucket=params["kwargs"]["aws_s3_bucket"].values["bucket"], Key=params["kwargs"]["object_key"])
-
-
-def is_possible_to_read_object(session, bucket_name, object_key):
-    try:
-        s3 = session.client("s3")
-        # read
-        s3.get_object(Bucket=bucket_name, Key=object_key)
-        return True
-
-    except Exception as e:
-        _logger.error(e)
-        return False
-
-
-def test_read_object(generate_params_for_test_read_object):
-    _logger.info("test_put_log_events")
-
-    params = generate_params_for_test_read_object
-    pattern = params["pattern"]
-
-    _logger.info({"pattern": pattern})
-
-    session = generate_boto3_session(params["kwargs"]["aws_iam_role"].values["arn"])
-
-    result = is_possible_to_read_object(
-        session, params["kwargs"]["aws_s3_bucket"].values["bucket"], params["kwargs"]["object_key"]
-    )
-
-    assert result == params["result"]
+    assert tester.test(aws_iam_role.values["arn"]) == expect
