a486653f2524bd8416c3a4f817351fed3401567d
Hikaru <hkak03key@gmail.com>
2024-02-08 17:31:53 +0000

    address to for_each/count

--- scripts/tf_pytest/terraform/sample.tf
+++ scripts/tf_pytest/terraform/sample.tf
@@ -1,5 +1,6 @@
 resource "time_static" "default" {}
 
+
 resource "time_static" "count" {
   count = 2
 }
@@ -8,6 +9,7 @@ resource "time_static" "count" {
 resource "time_static" "for_each" {
   for_each = toset(["one", "two"])
 }
+
 
 module "some_module" {
   source = "./modules/some_module"
@@ -27,7 +29,32 @@ module "some_module_for_each" {
   source = "./modules/some_module"
 }
 
+
 data "http" "default" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+
+data "http" "count" {
+  count = 2
+
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+
+data "http" "for_each" {
+  for_each = toset(["one", "two"])
+
   url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
 
   # Optional request headers
--- scripts/tf_pytest/tf_pytest/tfstate.py
+++ scripts/tf_pytest/tf_pytest/tfstate.py
@@ -1,7 +1,9 @@
 import json
 import logging
 import os
+import re
 from abc import ABC, abstractmethod
+from collections import UserList
 
 from .utility import exec_cmd as _exec_cmd
 
@@ -10,16 +12,19 @@ _logger = logging.getLogger(__name__)
 _logger.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
 
 
+# =================================================================
+# Node
 class Node(ABC):
     @abstractmethod
     def __init__(self, address, state):
         self.address = address
         self._state = state
 
+        _logger.debug("creating {}: {}".format(type(self), self))
+
     def __str__(self):
         return {
             "address": self.address,
-            "state": self._state,
         }.__str__()
 
     def __repr__(self):
@@ -29,49 +34,175 @@ class Node(ABC):
         }.__repr__()
 
 
-class NodeResources(Node):
+class NodeResourceType(Node):
+    def __init__(self, address, state):
+        self.resource_type = address.split(".")[-1]
+        super().__init__(address, state)
+
+    def __str__(self):
+        return {
+            "address": self.address,
+            "resource_type": self.resource_type,
+        }.__str__()
+
+    def __repr__(self):
+        return {
+            "address": self.address,
+            "state": self._state,
+            "resource_type": self.resource_type,
+        }.__repr__()
+
+
+# =================================================================
+# NodeResourceType
+class NodeResourceTypeResource(NodeResourceType):
     def __init__(self, address, state):
         super().__init__(address, state)
 
     def __getattr__(self, name):
-        _logger.debug("name: {}".format(name))
         target_addr = (self.address + "." + name).removeprefix(".")
         _logger.debug("target_addr: {}".format(target_addr))
 
-        _logger.debug("self._state: {}".format(self._state))
+        resources = [resource for resource in self._state["resources"] if resource["name"] == name]
+        if len(resources) == 1:
+            return NodeInstanceResource(target_addr, resources[0], self.resource_type)
 
-        new_state = [resource for resource in self._state["resources"] if resource["name"] == name]
-        if len(new_state) == 1:
-            return NodeResource(target_addr, new_state[0])
-
-        if len(new_state) >= 2:
-            return NodeResource(target_addr, new_state)
+        if len(resources) >= 2:
+            node_list = NodeListInstanceResource(
+                [
+                    NodeInstanceResource(
+                        (
+                            target_addr + "[{}]".format(r["index"])
+                            if type(r["index"]) == int
+                            else target_addr + '["{}"]'.format(r["index"])
+                        ),
+                        r,
+                        self.resource_type,
+                    )
+                    for r in resources
+                ]
+            )
+            return node_list
 
         raise AttributeError("{} is not found".format(target_addr))
 
 
-class NodeDatas(Node):
+class NodeResourceTypeData(NodeResourceTypeResource):
     def __init__(self, address, state):
         super().__init__(address, state)
 
     def __getattr__(self, name):
-        _logger.debug("name: {}".format(name))
         target_addr = (self.address + "." + name).removeprefix(".")
         _logger.debug("target_addr: {}".format(target_addr))
 
-        _logger.debug("self._state: {}".format(self._state))
+        resources = [resource for resource in self._state["resources"] if resource["name"] == name]
+        if len(resources) == 1:
+            return NodeInstanceData(target_addr, resources[0], self.resource_type)
+
+        if len(resources) >= 2:
+            node_list = NodeListInstanceData(
+                [
+                    NodeInstanceData(
+                        (
+                            target_addr + "[{}]".format(r["index"])
+                            if type(r["index"]) == int
+                            else target_addr + '["{}"]'.format(r["index"])
+                        ),
+                        r,
+                        self.resource_type,
+                    )
+                    for r in resources
+                ]
+            )
+            return node_list
+
+        raise AttributeError("{} is not found".format(target_addr))
+
+
+# =================================================================
+# NodeCategory
+class NodeCategory(Node):
+    def __init__(self, address, state):
+        self.category = address.split(".")[-1]
+        super().__init__(address, state)
+
+    def __str__(self):
+        return {
+            "address": self.address,
+            "category": self.category,
+        }.__str__()
+
+    def __repr__(self):
+        return {
+            "address": self.address,
+            "state": self._state,
+            "category": self.category,
+        }.__repr__()
+
+
+class NodeCategoryData(NodeCategory):
+    def __init__(self, address, state):
+        super().__init__(address, state)
+
+    def __getattr__(self, name):
+        target_addr = (self.address + "." + name).removeprefix(".")
+        _logger.debug("target_addr: {}".format(target_addr))
 
         new_state = {"resources": [resource for resource in self._state["resources"] if resource["type"] == name]}
-        _logger.debug("new_state: {}".format(new_state))
         if len(new_state["resources"]) == 0:
             raise AttributeError("{} is not found".format(target_addr))
 
-        return NodeResources(target_addr, new_state)
+        return NodeResourceTypeData(target_addr, new_state)
 
 
-class NodeResource(Node):
+class NodeCategoryModule(NodeCategory):
     def __init__(self, address, state):
         super().__init__(address, state)
+
+    def __getattr__(self, name):
+        target_addr = (self.address + "." + name).removeprefix(".")
+        _logger.debug("target_addr: {}".format(target_addr))
+
+        modules = [
+            child_module
+            for child_module in self._state["child_modules"]
+            if child_module["address"].startswith(target_addr)
+        ]
+
+        if len(modules) == 1:
+            return NodeInstanceModule(target_addr, resources[0], self.category)
+
+        if len(modules) >= 2:
+            node_list = NodeListInstanceModule([NodeInstanceModule(m["address"], m, self.category) for m in modules])
+            return node_list
+
+        raise AttributeError("{} is not found".format(target_addr))
+
+
+# =================================================================
+# NodeInstance
+class NodeInstance(Node):
+    def __init__(self, address, state, resource_type):
+        self.resource_type = resource_type
+        super().__init__(address, state)
+
+    def __str__(self):
+        return {
+            "address": self.address,
+            "resource_type": self.resource_type,
+        }.__str__()
+
+    def __repr__(self):
+        return {
+            "address": self.address,
+            "state": self._state,
+            "resource_type": self.resource_type,
+        }.__repr__()
+
+
+class NodeInstanceResource(NodeInstance):
+    def __init__(self, address, state, resource_type):
+        super().__init__(address, state, resource_type)
 
     def __getattr__(self, name):
         _logger.debug("name: {}".format(name))
@@ -79,102 +210,103 @@ class NodeResource(Node):
         if name in self._state.keys():
             return self._state[name]
 
-    def __getitem__(self, index):
-        _logger.debug("index: {}".format(index))
 
-        target_addr = (
-            (self.address + "[" + str(index) + "]").removeprefix(".")
-            if type(index) is int
-            else (self.address + '["' + index + '"]').removeprefix(".")
-        )
-        _logger.debug("target_addr: {}".format(target_addr))
-
-        _logger.debug("self._state: {}".format(self._state))
-
-        for resource in self._state:
-            if resource["index"] == index:
-                return NodeResource(target_addr, resource)
-
-        raise AttributeError("{} is not found".format(target_addr))
-
-
-class NodeModules(Node):
-    def __init__(self, address, state):
-        super().__init__(address, state)
+class NodeInstanceData(NodeInstance):
+    def __init__(self, address, state, resource_type):
+        super().__init__(address, state, resource_type)
 
     def __getattr__(self, name):
         _logger.debug("name: {}".format(name))
-        target_addr = (self.address + "." + name).removeprefix(".")
-        _logger.debug("target_addr: {}".format(target_addr))
 
-        _logger.debug("self._state: {}".format(self._state))
-
-        new_state = [
-            child_module
-            for child_module in self._state["child_modules"]
-            if child_module["address"].startswith(target_addr)
-        ]
-        if len(new_state) == 1:
-            return NodeModule(target_addr, new_state[0])
-
-        if len(new_state) >= 2:
-            return NodeModule(target_addr, new_state)
-
-        raise AttributeError("{} is not found".format(target_addr))
+        if name in self._state.keys():
+            return self._state[name]
 
 
-class NodeModule(Node):
-    def __init__(self, address, state):
-        super().__init__(address, state)
+class NodeInstanceModule(NodeInstance):
+    def __init__(self, address, state, resource_type):
+        super().__init__(address, state, resource_type)
 
     def __getattr__(self, name):
-        _logger.debug("name: {}".format(name))
         target_addr = (self.address + "." + name).removeprefix(".")
         _logger.debug("target_addr: {}".format(target_addr))
-
-        _logger.debug("self._state: {}".format(self._state))
 
         if name == "module":
-            return NodeModules(target_addr, {"child_modules": self._state["child_modules"]})
+            return NodeCategoryModule(target_addr, {"child_modules": self._state["child_modules"]})
 
         if name == "data":
-            return NodeDatas(
+            return NodeCategoryData(
                 target_addr,
                 {"resources": [resource for resource in self._state["resources"] if resource["mode"] == "data"]},
             )
 
         new_state = {"resources": [resource for resource in self._state["resources"] if resource["type"] == name]}
-        _logger.debug("new_state: {}".format(new_state))
         if len(new_state["resources"]) == 0:
             raise AttributeError("{} is not found".format(target_addr))
 
-        return NodeResources(target_addr, new_state)
-
-    def __getitem__(self, index):
-        _logger.debug("index: {}".format(index))
-
-        target_addr = (
-            (self.address + "[" + str(index) + "]").removeprefix(".")
-            if type(index) is int
-            else (self.address + '["' + index + '"]').removeprefix(".")
-        )
-        _logger.debug("target_addr: {}".format(target_addr))
-
-        _logger.debug("self._state: {}".format(self._state))
-
-        new_state = [child_module for child_module in self._state if child_module["address"].startswith(target_addr)]
-        if len(new_state) == 1:
-            return NodeModule(target_addr, new_state[0])
-
-        if len(new_state) >= 2:
-            raise AttributeError("{} exists multiple, this is bug or state is broken...".format(target_addr))
-
-        raise AttributeError("{} is not found".format(target_addr))
+        return NodeResourceTypeResource(target_addr, new_state)
 
 
-class NodeRoot(NodeModule):
+class NodeInsanceModuleRoot(NodeInstanceModule):
     def __init__(self):
         cwd = os.environ.get("TF_PYTEST_DIR", "../terraform")
         state = json.loads(_exec_cmd(["terraform", "show", "-json"], cwd=cwd, print_stderr=True).stdout.decode("utf8"))
 
-        super().__init__("", state["values"]["root_module"])
+        super().__init__("", state["values"]["root_module"], "module")
+
+
+def NodeRoot() -> NodeInsanceModuleRoot:
+    return NodeInsanceModuleRoot()
+
+
+# =================================================================
+# NodeList
+# MEMO: for_eachはUserListではなくUserDictを継承するべきかもしれない
+class NodeList(UserList):
+    def __init__(self, initlist=None):
+        super().__init__(initlist)
+
+        # check type
+        for node in self.data:
+            if not isinstance(node, Node):
+                raise ValueError("node must be NodeInstance")
+
+    def __iter__(self):
+        return self.data
+
+    def map(self, f):
+        return list(map(f, self.data))
+
+    def __getitem__(self, index):
+        for node in self.data:
+            if node._state["index"] == index:
+                return node
+
+        raise IndexError("index {} is not found".format(index))
+
+
+class NodeListInstanceResource(NodeList):
+    def __init__(self, initlist=None):
+        super().__init__(initlist)
+
+
+class NodeListInstanceData(NodeList):
+    def __init__(self, initlist=None):
+        super().__init__(initlist)
+
+
+class NodeListInstanceModule(NodeList):
+    def __init__(self, initlist=None):
+        super().__init__(initlist)
+
+    def __getitem__(self, index):
+        _logger.debug("index: {}".format(index))
+        for node in self.data:
+            node_index = re.match(r".*\[(.+)\]$", node.address).group(1)
+
+            if type(index) == int and node_index == str(index):
+                return node
+
+            if type(index) == str and node_index == f'"{index}"':
+                return node
+
+        raise IndexError("index {} is not found".format(index))
