077cf600370e8c87aacf5d4f11b40db010e23234
Hikaru <hkak03key@gmail.com>
2024-02-13 03:04:24 +0000

    replace this to default

--- aws/terraform/modules/aws_cloudwatch_log_group/test/terraform/_aws_info.tf
+++ aws/terraform/modules/aws_cloudwatch_log_group/test/terraform/_aws_info.tf
@@ -1,10 +1,10 @@
-data "aws_caller_identity" "this" {}
+data "aws_caller_identity" "default" {}
 
-data "aws_region" "this" {}
+data "aws_region" "default" {}
 
 locals {
   #======================
   # aws情報
-  aws_account_id = data.aws_caller_identity.this.account_id
-  region         = data.aws_region.this.name
+  aws_account_id = data.aws_caller_identity.default.account_id
+  region         = data.aws_region.default.name
 }
