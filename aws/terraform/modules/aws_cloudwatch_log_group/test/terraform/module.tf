b3b646ad692d824129ee3aeb946239daa6208240
Hikaru <hkak03key@gmail.com>
2024-03-10 07:02:57 +0000

    aws_cloudwatch_log_group

--- aws/terraform/modules/aws_cloudwatch_log_group/test/terraform/module.tf
+++ aws/terraform/modules/aws_cloudwatch_log_group/test/terraform/module.tf
@@ -1,8 +1,6 @@
 module "default" {
   source = "../../"
 
-  system_name              = local.system_name
-  env                      = var.env
-  aws_iam_policy_infos_dir = local.aws_iam_policy_infos_dir
-  name_prefix              = local.name_prefix
+  _system_info              = local._system_info
+  _module_hierarchical_info = local._module_hierarchical_info
 }
