6f80ccc72701cbbf89226f734c542cc4801338d4
Hikaru <hkak03key@gmail.com>
2024-03-14 15:45:19 +0000

    def module

---
+++ aws/terraform/roots/admin/modules/aws_iam_openid_connect_provider_github_actions/variable.tf
@@ -0,0 +1,29 @@
+variable "_system_info" {
+  type = object({
+    env                      = string
+    aws_iam_policy_infos_dir = string
+    name_prefix              = string
+    long_name_prefix         = string
+  })
+}
+
+variable "_module_hierarchical_info" {
+  type = list(
+    object({
+      module_name                = string
+      append_module_name_to_name = bool
+      name_suffix                = string
+    })
+  )
+}
+
+
+variable "name_suffix" {
+  type    = string
+  default = ""
+
+  description = <<DESC
+リソース名に設定されるsuffix。
+命名ルールは locals.tf の命名セクションを参照。
+DESC
+}
