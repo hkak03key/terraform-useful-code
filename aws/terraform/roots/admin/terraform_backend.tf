aedad575074ec434d98ade1391f0019f561bc232
Hikaru <hkak03key@gmail.com>
2024-03-21 07:15:35 +0000

    impl tf_backend for ci_tf_module

--- aws/terraform/roots/admin/terraform_backend.tf
+++ aws/terraform/roots/admin/terraform_backend.tf
@@ -36,12 +36,34 @@ module "terraform_backend_admin" {
 }
 
 
-moved {
-  from = module.terraform_backend_admin.module.aws_iam_role_github_actions_ci
-  to   = module.terraform_backend_admin.module.aws_iam_role_github_actions["ci"]
-}
+module "terraform_backend_ci_tf_module" {
+  source = "./modules/terraform_backend"
 
-moved {
-  from = module.terraform_backend_admin.module.aws_iam_role_github_actions_deploy
-  to   = module.terraform_backend_admin.module.aws_iam_role_github_actions["deploy"]
+  # fixed variables
+  _system_info              = local._system_info
+  _module_hierarchical_info = local._module_hierarchical_info
+
+  # set name_suffix if needed
+  name_suffix = "ci_tf_module"
+
+  # add more variables here
+  admin_aws_iam_principals = local.admin_aws_iam_principals
+  readwrite_aws_iam_principals = flatten([
+    local.admin_aws_iam_principals,
+    module.terraform_backend_ci_tf_module.aws_iam_role_github_actions["default"].aws_iam_role,
+  ])
+
+  read_aws_iam_principals = [
+  ]
+
+  aws_iam_role_github_actions_config = {
+    default = {
+      name_suffix                     = null
+      aws_iam_openid_connect_provider = module.aws_iam_openid_connect_provider_github_actions.aws_iam_openid_connect_provider
+      aws_iam_policies = [
+        data.aws_iam_policy.aws_managed["AdministratorAccess"],
+      ]
+      is_output = true
+    }
+  }
 }
