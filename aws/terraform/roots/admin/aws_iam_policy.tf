781af596c0e0c44a245a231310b5fa8e8b5d15b5
Hikaru <hkak03key@gmail.com>
2024-03-24 17:41:24 +0000

    fix policy

--- aws/terraform/roots/admin/aws_iam_policy.tf
+++ aws/terraform/roots/admin/aws_iam_policy.tf
@@ -15,15 +15,33 @@ resource "aws_iam_policy" "administrator_access_for_ci_tf_module" {
     Version = "2012-10-17"
     Statement = [
       {
+        Sid      = "TagBasedAccess"
         Effect   = "Allow"
         Action   = "*"
         Resource = "*"
         Condition = {
           StringEquals = {
-            "aws:ResourceTag/env" : "ci-tf-module"
+            for k in [
+              "aws:RequestTag/env",
+              "aws:ResourceTag/env",
+            ] : k => "ci-tf-module"
           }
         }
       },
+      {
+        # Tag Based Accessができないリソース
+        Sid    = "ResourceNameBasedAccess"
+        Effect = "Allow"
+        Action = "*"
+        Resource = [
+          "arn:aws:kms:*:*:alias/ci-tf-module-*",
+          "arn:aws:dynamodb:*:*:table/ci-tf-module-*",
+          "arn:aws:s3:::${var.account_name != null ? var.account_name : local.aws_account_id}-ci-tf-module-*",
+          "arn:aws:iam::*:role/ci-tf-module-*",
+          "arn:aws:iam::*:policy/*-ci-tf-module-*",
+          "arn:aws:iam::*:policy/*-c-tf-mdl-*",
+        ]
+      }
     ]
   })
 }
